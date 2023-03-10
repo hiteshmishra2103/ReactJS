971a253addc084044e02bc535e19b3a7e9e1fc1b
Hitesh mishra <mishrahitesh854@gmail.com>
2023-03-10 12:17:43 +0530

    created checkout page along with checkout items and checkout cart functionality

--- crown-clothing/src/components/cart-icon/cart-dropdown/cart-dropdown.component.jsx
+++ crown-clothing/src/components/cart-icon/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,8 @@
 
 import { useContext } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../../contexts/cart.context";
 
 import CartItem from "../../cart-item/cart-item.component";
@@ -12,6 +14,13 @@ import Button from "../../button/button.component";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+
+  const navigate = useNavigate();
+
+  const goToCheckout = () => {
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -19,8 +28,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-
-      <Button>Go To Checkout</Button>
+      <Button onClick={goToCheckout}>Go To Checkout</Button>
     </div>
   );
 };
--- crown-clothing/src/contexts/cart.context.jsx
+++ crown-clothing/src/contexts/cart.context.jsx
@@ -21,12 +21,44 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+//following function is to remove the item from cart
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  //find the cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  //if quantity is equal to 1, then remove that item from the cart and return the new array without
+  //that item
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  //if above conditions don't met then return the cartItems with matching cartItem with reduced
+  //quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+
+//function to clearing the cartItem completely
+const clearCartItem = (cartItems, cartItemToClear) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal: 0,
 });
 
 /*
@@ -45,6 +77,7 @@ export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   useEffect(() => {
     //following function keeps the record of count of cart items
@@ -55,17 +88,37 @@ export const CartProvider = ({ children }) => {
     setCartCount(newCartCount);
   }, [cartItems]);
 
+  useEffect(() => {
+    //following function keeps the record of count of cart items
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+    setCartTotal(newCartTotal);
+  }, [cartItems]);
+
   //below function is going to trigger whenever the user clicks on the addToCart button
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const removeItemToCart = (cartItemToRemove) => {
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  };
+
+  const clearItemFromCart = (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
   };
 
   const value = {
     isCartOpen,
     setIsCartOpen,
     addItemToCart,
+    removeItemToCart,
+    clearItemFromCart,
     cartItems,
     cartCount,
+    cartTotal,
   };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
